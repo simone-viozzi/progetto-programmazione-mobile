0dccb38972f1b9e05275fe898c75119be0c300ef
simone viozzi <viozzis333@gmail.com>
2021-10-21 16:06:12 +0200

    adding comments to shared pref app

---
+++ sharedPref/README.md
@@ -0,0 +1,10 @@
+# sharedPrefs
+
+## topic of the app
+- view binding
+- shared preference
+- an extension function
+- constraint layout guidelines
+
+## screen of the app
+![img](screen.jpeg)
--- sharedPref/app/src/main/java/com/example/sharedpref/MainActivity.kt
+++ sharedPref/app/src/main/java/com/example/sharedpref/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.sharedpref.databinding.ActivityMainBinding
 
 
+/**
+ * To editable -> an extension function of string to set the text of an editText in one line
+ * (because editText.text is Editable, so i need to convert a String to an Editable)
+ */
 fun String.toEditable(): Editable =  Editable.Factory.getInstance().newEditable(this)
 
 
@@ -20,6 +24,7 @@ class MainActivity : AppCompatActivity()
 
         val sharedPref = getPreferences(Context.MODE_PRIVATE)
 
+        // take the values auto of the sharedPref and set them in the UI
         with(binding) {
             editTextString.text = sharedPref.getString(getString(R.string.string_pref), "")?.toEditable()
             editTextInt.text = sharedPref.getInt(getString(R.string.int_pref), 0).toString().toEditable()
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity()
             toggleButtonBoolean.isChecked = sharedPref.getBoolean(getString(R.string.boolean_pref), false)
         }
 
+        // when the button get pressed take the data out of the UI and set it into the sharedPref
         binding.button.setOnClickListener {
             with (sharedPref.edit()) {
                 putString(getString(R.string.string_pref), binding.editTextString.text.toString())
@@ -34,6 +40,7 @@ class MainActivity : AppCompatActivity()
                 putFloat(getString(R.string.float_pref), binding.editTextFloat.text.toString().toFloat())
                 putBoolean(getString(R.string.boolean_pref), binding.toggleButtonBoolean.isChecked)
 
+                // remember to call apply at the end!
                 apply()
             }
 
