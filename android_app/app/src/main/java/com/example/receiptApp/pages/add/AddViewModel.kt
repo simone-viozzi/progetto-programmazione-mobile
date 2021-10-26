f84ef97bc967a1ebda9bdf10767fd697af282133
simone viozzi <viozzis333@gmail.com>
2021-10-26 10:44:48 +0200

    made mutable live data privare and live data public

--- android_app/app/src/main/java/com/example/receiptApp/pages/add/AddViewModel.kt
+++ android_app/app/src/main/java/com/example/receiptApp/pages/add/AddViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.receiptApp.pages.add
 
 import android.text.format.DateFormat
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,7 +9,10 @@ import androidx.lifecycle.ViewModel
 class AddViewModel : ViewModel()
 {
     // the list observed by the recyclerview
-    val rvList = MutableLiveData<List<AddDataModel>>()
+    private val _rvList = MutableLiveData<List<AddDataModel>>()
+    val rvList: LiveData<List<AddDataModel>>
+        get() = _rvList
+
 
     // the callback used by every element of the views in the textWatcher to update the corresponding element in the
     // view model, it pass an element with only the updated field and the id != null
@@ -19,19 +23,19 @@ class AddViewModel : ViewModel()
         {
             is AddDataModel.Header ->
             {
-                val oldEl = rvList.value?.get(0) as AddDataModel.Header
+                val oldEl = _rvList.value?.get(0) as AddDataModel.Header
 
                 // TODO (el... != "") in sill needed? need to test
                 if (el.date != null && el.date != "") oldEl.date = el.date
                 if (el.tag != null && el.tag != "") oldEl.tag = el.tag
 
                 // TODO update the list only if there was a change!
-                rvList.value = rvList.value?.toMutableList().also { it?.set(0, oldEl) }
+                _rvList.value = _rvList.value?.toMutableList().also { it?.set(0, oldEl) }
             }
 
             is AddDataModel.SingleElement ->
             {
-                val oldEl = rvList.value?.get(el.id) as AddDataModel.SingleElement
+                val oldEl = _rvList.value?.get(el.id) as AddDataModel.SingleElement
 
                 // TODO (el... != "") in sill needed? need to test
                 if (el.name != null && el.name != "") oldEl.name = el.name
@@ -39,16 +43,16 @@ class AddViewModel : ViewModel()
                 if (el.num != null) oldEl.num = el.num
                 if (el.cost != null) oldEl.cost = el.cost
 
-                val newList = rvList.value?.toMutableList().also { it?.set(el.id, oldEl) }
+                val newList = _rvList.value?.toMutableList().also { it?.set(el.id, oldEl) }
 
                 if (el.id == getLastId(false) - 1)
                 {
                     // TODO update the list only if there was a change!
-                    rvList.value = newList?.plus(listOf(AddDataModel.SingleElement(id = getLastId())))
+                    _rvList.value = newList?.plus(listOf(AddDataModel.SingleElement(id = getLastId())))
                 } else
                 {
                     // TODO update the list only if there was a change!
-                    rvList.value = newList!!
+                    _rvList.value = newList!!
                 }
             }
         }
@@ -58,7 +62,7 @@ class AddViewModel : ViewModel()
     fun setDate(millis: Long)
     {
         val date = DateFormat.format("dd/MM/yyyy", millis).toString()
-        rvList.value = rvList.value?.toMutableList().also { li ->
+        _rvList.value = _rvList.value?.toMutableList().also { li ->
             val header = li?.get(0) as AddDataModel.Header
             li[0] = header.also { it.date = date }
         }
@@ -78,7 +82,7 @@ class AddViewModel : ViewModel()
 
     init
     {
-        rvList.value = listOf(
+        _rvList.value = listOf(
             AddDataModel.Header(id = 0),
             AddDataModel.SingleElement(id = getLastId())
         )
