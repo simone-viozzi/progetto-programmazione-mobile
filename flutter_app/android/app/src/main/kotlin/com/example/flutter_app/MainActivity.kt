a373d4becc2c271672892c0cd417b03cd848d9e0
SvMax <Biancucci95@gmail.com>
2021-11-29 22:43:17 +0100

    disperazione

--- flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.flutter_app
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+    }
 
 }
